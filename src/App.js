19f7d99fafb51b70ab3e75f0f036400503b14e3a
Sam Singer <samuel.singer1@gmail.com>
2019-03-27 01:12:31 -0700

    State now added to the app

--- src/App.js
+++ src/App.js
@@ -4,12 +4,19 @@ import UserInput from './UserInput/UserInput';
 import UserOutput from './UserOutput/UserOutput.js';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      username: "Sam"
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Assignment 1</h1>
         <UserInput />
-        <UserOutput content="Output 100" username="Sam"/>
+        <UserOutput content="Output 100" username={this.state.username}/>
         <UserOutput content="Output 200" />
         <UserOutput content="Output 300" />
       </div>
