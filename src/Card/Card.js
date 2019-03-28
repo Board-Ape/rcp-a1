cb294373a9ce68ee416d2edfc12fdaefce2922b1
Sam Singer <samuel.singer1@gmail.com>
2019-03-28 01:05:21 -0700

    Assignment complete

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import './Card.css'
 
 const card = (props) => {
-    return props.todos.map(todo => {
-        return(
-            <div>
-                {todo}
-            </div>
-        )
+    return props.todos.map((todo, key) => {
+        return (
+          <div className="todo" key={key}>
+            <p contentEditable>{todo}</p>
+            <button onClick={(event) => props.delete(event, key)}>X</button>
+          </div>
+        );
     })
 }
 
